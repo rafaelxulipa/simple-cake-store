5ac141b7cd39874e66ff3398b4e3a180796ff743
Otavio Rafael <rafael2104@gmail.com>
2023-01-04 23:38:42 -0300

    logica das validações finalizadas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,9 @@
+var form = document.querySelector('form');
+
+$("#cakeForm").submit(function(event){
+    event.preventDefault();
+ });
+
 $('#inputDate').attr('min', new Date().toISOString().split("T")[0])
 
 $("#inputZipcode").focusout(function () {
@@ -19,64 +25,28 @@ $("#inputZipcode").focusout(function () {
     });
 });
 
-
-// (function () {
-//     'use strict'
-
-//     var forms = document.querySelectorAll('.needs-validation')
-
-//     Array.prototype.slice.call(forms)
-//       .forEach(function (form) {
-//         form.addEventListener('submit', function (event) {
-//           if (!form.checkValidity()) {
-//             event.preventDefault()
-//             event.stopPropagation()
-//           }
-
-//           form.classList.add('was-validated')
-//         }, false)
-//       })
-//   })();
-
-var form = document.querySelector('form');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-});
-
-$("#firstName").on('change', function() { 
-
-    if(!$.trim($("#firstName").val())) {
-       $("#lastName").prop('disabled', true);
-       console.log('vazio');
-     } else {
-           $("#lastName").prop('disabled', false);
-       }
- });
-
-
-
 $('#btnSubmit').click(function () {
-
-    $("#cakeForm").validate();
 
     var formEvaluation = $('#cakeForm');
 
-
-    $.ajax({
-
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        type: "post",
-        dataType: "json",
-        data: formEvaluation.serialize(),
-        success: function (formEvaluation) {
-            alert('PEDIDO ENVIADO COM SUCESSO');
-            //Log do JSON que envio visa ajax
-            console.log(formEvaluation);
-        },
-        error: function () {
-            alert('ok erro');
-        },
-    });
-
+    if (form.checkValidity()){
+        $.ajax({
+    
+            url: 'https://jsonplaceholder.typicode.com/posts',
+            type: "POST",
+            dataType: "json",
+            data: formEvaluation.serialize(),
+            success: function (formEvaluation) {
+                alert('PEDIDO ENVIADO COM SUCESSO');
+                
+                //Log do JSON que envio via ajax
+                console.log(formEvaluation);
+            },
+            error: function () {
+                alert('ERRO AO ENVIAR');
+            },
+        });
+    } else {
+        form.classList.add('was-validated')
+    }
 });
